9053a8f8c01d9debc5111113a076cbf41a4e4936
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-15 13:01:27 -0500

    set up spy profile display and name prompt.

--- activities/03-spy-profile-generator/js/script.js
+++ activities/03-spy-profile-generator/js/script.js
@@ -8,6 +8,13 @@ A vanilla spy profile generator using JSON data sets.
 "use strict";
 
 
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  seacretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`,
+};
+
 /**
 Description of preload
 */
@@ -20,13 +27,28 @@ function preload() {
 Description of setup
 */
 function setup() {
+  createCanvas(windowWidth, windowHeight);
 
+  spyProfile.name = prompt(`Agent! What is your name?`);
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(255);
 
+  let profile = `**SPY PROFILE! DO NOT DIVULGE! **
+  Name: ${spyProfile.name}
+  Alias: ${spyProfile.alias}
+  Seacret Weapon: ${spyProfile.seacretWeapon}
+  password: ${spyProfile.password}`;
+
+  push();
+  textFont(`courier, monospace`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(0);
+  text(profile, 100, 100);
+  pop();
 }
