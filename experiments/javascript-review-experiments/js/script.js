11ed53ad66a11ff2c4753a096234b17f146585cb
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-14 19:36:00 -0500

    review: copied example code for constants review experiment.

--- experiments/javascript-review-experiments/js/script.js
+++ experiments/javascript-review-experiments/js/script.js
@@ -12,7 +12,9 @@ These are warm up experimens on basic javascript techniques done in class and by
 Description of preload
 */
 function preload() {
-
+  const NUM_CIRCLES = 10;
+  let circleSizeIncrease = 50;
+  let circleAlpha = 50;
 }
 
 
@@ -20,7 +22,7 @@ function preload() {
 Description of setup
 */
 function setup() {
-
+  createCanvas(500, 500);
 }
 
 
@@ -28,5 +30,17 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  background(0);
 
+  // Just for fun, since they ARE variables after all
+  circleAlpha = map(mouseX, 0, width, 10, 100);
+  circleSizeIncrease = map(mouseY, 0, height, 10, 100);
+
+  // Loop once for each of 10 circles, drawing them 50 pixels larger each time
+  for (let i = 0; i < NUM_CIRCLES; i++) {
+    push();
+    fill(255, circleAlpha);
+    ellipse(width / 2, height / 2, i * circleSizeIncrease);
+    pop();
+  }
 }
