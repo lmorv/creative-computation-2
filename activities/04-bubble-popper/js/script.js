18ec7595b67309e9abbdda1daf314167616b2931
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-15 16:51:44 -0500

    started bubble popping activity.

---
+++ activities/04-bubble-popper/js/script.js
@@ -0,0 +1,22 @@
+/**
+bubble Popper
+Leo Morales
+
+A program about popping bubbles using the computer's camera and object detection AI
+*/
+
+"use strict";
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
