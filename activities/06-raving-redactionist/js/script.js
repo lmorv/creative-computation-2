5ce80ef21367e4a15404c31f5cb0050a70a307a6
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-16 17:58:16 -0500

    started raving redactionist activity.

---
+++ activities/06-raving-redactionist/js/script.js
@@ -0,0 +1,32 @@
+/**
+Raving Redactionist
+Leonardo Morales
+
+A project about a raving redactionist presumably, using JQuery tecniques in some usefull way
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
