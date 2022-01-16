9f338f9f43c40932ea696b76289f474778745f58
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-16 15:10:44 -0500

    started haiku generator activity.

---
+++ activities/05-haiku-generator/js/script.js
@@ -0,0 +1,31 @@
+/**
+Hiku Generator
+Leonardo Morales
+
+A haiku generator using html and the DOM
+*/
+
+"use strict";
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
