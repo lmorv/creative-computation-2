493df1f208939b93afb8a0e007d913fb1dc61fe1
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-16 21:54:54 -0500

    started code taker activity.

---
+++ activities/07-code-taker/js/script.js
@@ -0,0 +1,32 @@
+/**
+Code Taker
+Leonardo Morales
+
+A program about breaking codes hidden in poems!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
