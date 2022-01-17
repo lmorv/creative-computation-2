e98de923bc22365e2c49f2f844d98a6a99d190b9
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-16 22:51:44 -0500

    finished code taker activity.

--- activities/07-code-taker/js/script.js
+++ activities/07-code-taker/js/script.js
@@ -2,31 +2,36 @@
 Code Taker
 Leonardo Morales
 
-A program about breaking codes hidden in poems!
+A program about breaking codes hidden in poems! Da Vinci?!
 */
 
 "use strict";
 
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+  buttons: {
+    "I know.": function() {
+      $(this).dialog(`close`);
+    }
+  }
+});
 
-/**
-Description of preload
-*/
-function preload() {
+$(`.secret`).one(`mouseover`, function(event) {
+  $(this).addClass(`found`, 500);
+  $(this).draggable({
+    helper: `clone`
+  });
+});
 
-}
-
-
-/**
-Description of setup
-*/
-function setup() {
-
-}
-
-
-/**
-Description of draw()
-*/
-function draw() {
-
-}
+$(`#answer`).droppable({
+  drop: function(event, ui) {
+    let letter = ui.draggable.text();
+    $(this).append(letter);
+    ui.draggable.draggable(`disable`);
+    ui.draggable.removeClass(`found`);
+    // check if the user has the correct answer
+    if ($(this).text() === `Theremin`) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+  }
+});
