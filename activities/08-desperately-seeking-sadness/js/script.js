d3c9d72e852271fe70ca0b2733e09c0dc29fbe96
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-17 21:17:08 -0500

    started desperately seeking sadness activity!

---
+++ activities/08-desperately-seeking-sadness/js/script.js
@@ -0,0 +1,32 @@
+/**
+Desperately Seeking Sadness
+Leonardo Morales
+
+A project to explore the Phaser game engine while desperately seeking sadness.
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
