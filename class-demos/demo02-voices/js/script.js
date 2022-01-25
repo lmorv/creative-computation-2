dfd912bdbe8cdfae4fe7efcf5e1e2f31b4c31a66
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-25 14:58:28 -0500

    classDemo02: following along with class experiments.

--- class-demos/demo02-voices/js/script.js
+++ class-demos/demo02-voices/js/script.js
@@ -1,9 +1,10 @@
 /**
-Title of Project
-Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+* annyang!
+  * general review of the process?
+* ResponsiveVoice
+  * Can you connect ResponsiveVoice to other libraries?
+
 */
 
 "use strict";
@@ -12,22 +13,47 @@ author, and this description to match your project!
 /**
 Description of preload
 */
+
+let face = `:-|`;
+
 function preload() {
 
 }
-
 
 /**
 Description of setup
 */
 function setup() {
+  createCanvas(500, 500);
+
+  if (annyang) {
+    let commands = {
+      "I am so happy": happy
+    };
+    annyang.addCommands(commands);
+    annyang.statrt();
+    annyang.debug(true);
+  } else {
+    // Annyang does not work!
+    alert(`Please visit this page in google chrome`);
+  }
+
 
 }
-
 
 /**
 Description of draw()
 */
 function draw() {
+  background(0);
+  push();
+  textSize(128);
+  fill(0);
+  text(face, width / 2, height / 2);
+  pop();
+
+  function happy() {
+    face = `:-)`;
+  }
 
 }
