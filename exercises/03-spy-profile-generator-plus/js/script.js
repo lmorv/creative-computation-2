7607a2efaf103e942847a769d644d522cbc612f3
lmorv <84216711+lmorv@users.noreply.github.com>
2022-01-26 23:40:06 -0500

    bublePopper: added base project files.

---
+++ exercises/03-spy-profile-generator-plus/js/script.js
@@ -0,0 +1,107 @@
+/**
+Spy Profile Generator
+Leonardo Morales
+
+A vanilla spy profile generator using JSON data sets.
+password is: resourcefulness
+*/
+
+"use strict";
+
+
+let spyProfile = {
+  name: `**REDACTED**`,
+  alias: `**REDACTED**`,
+  seacretWeapon: `**REDACTED**`,
+  password: `**REDACTED**`,
+};
+
+let instrumentData = undefined;
+let objectData = undefined;
+let tarotData = undefined;
+
+const instrumentURL = `https://raw.githubusercontent.com/dariusk/corpora/master/data/music/instruments.json`;
+const objectURL = `https://raw.githubusercontent.com/dariusk/corpora/master/data/objects/objects.json`;
+const tarotURL = `https://raw.githubusercontent.com/dariusk/corpora/master/data/divination/tarot_interpretations.json`;
+
+const savedProfileKey = `spy-profile-data`; // used to setItem and getIten from localStorage
+let data = JSON.parse(localStorage.getItem(savedProfileKey));
+
+/**
+Description of preload
+*/
+function preload() {
+  instrumentData = loadJSON(instrumentURL);
+  objectData = loadJSON(objectURL);
+  tarotData = loadJSON(tarotURL);
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  // check if there is saved profile data to load.
+  // if  the data variable is not null(empty) assign stored properties too spyProfile:
+  if (data) {
+    let password = prompt(`Agent! What is your password?!`);
+
+    if (password === data.password) {
+      setSpyData();
+      // spyProfile.name = data.name;
+      // spyProfile.alias = data.alias;
+      // spyProfile.seacretWeapon = data.seacretWeapon;
+      // spyProfile.password = data.password;
+    }
+  } else {
+    generateSpyProfile();
+  }
+}
+
+function setSpyData() {
+  // let data = JSON.parse(localStorage.getItem(`spy-profile-data`)); // I have to declare the data variable again here... seems wrong.
+
+  spyProfile.name = data.name;
+  spyProfile.alias = data.alias;
+  spyProfile.seacretWeapon = data.seacretWeapon;
+  spyProfile.password = data.password;
+}
+
+function generateSpyProfile() {
+  // prompt user for their name:
+  spyProfile.name = prompt(`Agent! What is your name?`);
+  // generate profile from data sets
+  let instrument = random(instrumentData.instruments); // stores a random instrument
+  spyProfile.alias = `The ${instrument}`; // ads the word `The` to the string
+
+  spyProfile.seacretWeapon = random(objectData.objects);
+
+  let card = random(tarotData.tarot_interpretations); // select a random card object
+  spyProfile.password = random(card.keywords); // access that card's keywords array and select a random item.
+
+  // save the user's profile info:
+  localStorage.setItem(savedProfileKey, JSON.stringify(spyProfile));
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(255);
+
+  let profile = `**SPY PROFILE! DO NOT DIVULGE! **
+  Name: ${spyProfile.name}
+  Alias: ${spyProfile.alias}
+  Seacret Weapon: ${spyProfile.seacretWeapon}
+  password: ${spyProfile.password}`;
+
+  push();
+  textFont(`courier, monospace`);
+  textSize(24);
+  textAlign(LEFT, TOP);
+  fill(0);
+  text(profile, 100, 100);
+  pop();
+}
---
+++ exercises/04-bubble-popper-plus/js/script.js
@@ -0,0 +1,102 @@
+/**
+bubble Popper
+Leo Morales
+
+A program about popping bubbles with your index finger using the computer's camera and hand pose detection AI
+*/
+
+"use strict";
+
+// var to store the users web cam:
+let video = `undefined`;
+// the Handpose model:
+let handpose = `undefined`;
+// the current set of predictions:
+let predictions = [];
+// the bubble
+let bubble = undefined;
+/**
+Description of setup
+*/
+function setup() {
+  createCanvas(640, 480);
+
+  // Access user's web cam:
+  video = createCapture(VIDEO);
+  video.hide();
+
+  // load the Handpose model
+  handpose = ml5.handpose(video, {
+    flipHorizontal: true
+  }, function() {
+    console.log(`Model loaded.`);
+  });
+
+  // listen for predictions
+  handpose.on(`predict`, function(results) {
+    console.log(results);
+    predictions = results;
+  });
+
+  // Our bubble
+  bubble = {
+    x: random(width),
+    y: height,
+    size: 90,
+    vx: 0,
+    vy: -2
+  };
+}
+
+/**
+Description of draw()
+*/
+function draw() {
+  background(0);
+
+  if (predictions.length > 0) {
+    let hand = predictions[0];
+    let index = hand.annotations.indexFinger;
+    let tip = index[3];
+    let base = index[0];
+    let tipX = tip[0];
+    let tipY = tip[1];
+    let baseX = base[0];
+    let baseY = base[1];
+    // pin
+    push()
+    noFill();
+    stroke(255, 255, 255);
+    strokeWeight(2);
+    line(baseX, baseY, tipX, tipY);
+    pop()
+    // pin base
+    push();
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(baseX, baseY, 20);
+    pop();
+
+    // check bubble popping
+    let d = dist(tipX, tipY, bubble.x, bubble.y);
+    if (d < bubble.size / 2) {
+      bubble.x = random(width);
+      bubble.y = height;
+    }
+  }
+
+  // bubble movement
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
+
+  if (bubble.y < 0) {
+    bubble.x = random(width);
+    bubble.y = height;
+  }
+
+  push();
+  fill(0, 200, 100);
+  noStroke();
+  ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+}
