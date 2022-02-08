bda118367878ee69a22259395ce2df7bd41fa981
lmorv <84216711+lmorv@users.noreply.github.com>
2022-02-08 13:56:06 -0500

    liveDemo-AI: Started AI class demo.

---
+++ class-demos/demo04-AI/js/script.js
@@ -0,0 +1,32 @@
+/**
+AI Class Demo
+Leonardo Morales
+
+Experiments in class using AI and the computer's camera.
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
