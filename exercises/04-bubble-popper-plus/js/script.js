ed3bb0dd03fa1acdbcc0a77cf1c0f474e662b0a2
lmorv <84216711+lmorv@users.noreply.github.com>
2022-02-08 16:22:13 -0500

    popperPlus: Moved code from draw() into functions added coments.

--- exercises/04-bubble-popper-plus/js/script.js
+++ exercises/04-bubble-popper-plus/js/script.js
@@ -80,38 +80,6 @@ function draw() {
     running();
   }
 
-  background(0);
-
-  if (predictions.length > 0) {
-    let hand = predictions[0];
-    let index = hand.annotations.indexFinger;
-    let tip = index[3];
-    let base = index[0];
-    let tipX = tip[0];
-    let tipY = tip[1];
-    let baseX = base[0];
-    let baseY = base[1];
-    // pin
-    push()
-    noFill();
-    stroke(255, 255, 255);
-    strokeWeight(2);
-    line(baseX, baseY, tipX, tipY);
-    pop()
-    // pin base
-    push();
-    noStroke();
-    fill(255, 0, 0);
-    ellipse(baseX, baseY, 20);
-    pop();
-
-    // check bubble popping
-    let d = dist(tipX, tipY, bubble.x, bubble.y);
-    if (d < bubble.size / 2) {
-      bubble.x = random(width);
-      bubble.y = height;
-    }
-  }
 
   // bubble movement
   bubble.x += bubble.vx;
@@ -129,11 +97,111 @@ function draw() {
   pop();
 }
 
+/**
+Displays a loading screen with the loading model's name
+*/
+
 function loading() {
   push();
   textSize(32);
   textStyle(BOLD);
   textAlign(CENTER, CENTER);
   text(`Loading ${modelName}...`, width / 2, height / 2);
+  pop();
+}
+
+function running() {
+  // Use these lines to see the video feed
+  // const flippedVideo = ml5.flipImage(video);
+  // image(flippedVideo, 0, 0, width, height);
+
+  // Use this line to see a black background
+  background(0);
+
+  // check if there are predictions to display
+  if (predictions.length > 0) {
+    // If yes, then get the positions of the tip and base of the index finger
+    updatePin(predictions[0]);
+
+    // check bubble popping
+    let d = dist(pin.tip.x, pin.tip.y, bubble.x, bubble.y);
+    if (d < bubble.size / 2) {
+      // pop! and reset bubble
+      resetBubble();
+    }
+    // Display the current position of the pin
+    displayPin();
+  }
+
+  // Handle the bubble's movement and display (independent of hand detection
+  // so it doesn't need to be inside the predictions check)
+  moveBubble();
+  checkOutOfBounds();
+  displayBubble();
+}
+
+/**
+Updates the position of the pin according to the latest prediction
+*/
+function updatePin(prediction) {
+  pin.tip.x = prediction.annotations.indexFinger[3][0];
+  pin.tip.y = prediction.annotations.indexFinger[3][1];
+  pin.head.x = prediction.annotations.indexFinger[0][0];
+  pin.head.y = prediction.annotations.indexFinger[0][1];
+}
+
+/**
+Resets the bubble to the bottom of the screen in a new x position
+*/
+function resetBubble() {
+  bubble.x = random(width);
+  bubble.y = height;
+}
+
+/**
+Moves the bubble according to its velocity
+*/
+function moveBubble() {
+  bubble.x += bubble.vx;
+  bubble.y += bubble.vy;
+}
+
+/**
+Resets the bubble if it moves off the top of the canvas
+*/
+function checkOutOfBounds() {
+  if (bubble < 0) {
+    resetBubble();
+  }
+}
+
+/**
+Displays the bubble as a circle
+*/
+function displayBubble() {
+  push();
+  noStroke();
+  fill(100, 100, 200, 150);
+  ellipse(bubble.x, bubble.y, bubble.size);
+  pop();
+}
+
+/**
+Displays the pin based on the tip and base coordinates. Draws
+a line between them and adds a red pinhead.
+*/
+function displayPin() {
+  // draw pin
+  push()
+  noFill();
+  stroke(255, 255, 255);
+  strokeWeight(2);
+  line(pin.tip.x, pin.tip.y, pin.head.x, pin.head.y);
+  pop()
+  //  draw pin head
+  push();
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(pin.head.x, pin.head.y, pin.head.size);
   pop();
 }
