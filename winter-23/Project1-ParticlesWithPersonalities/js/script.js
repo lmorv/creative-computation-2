3efa536d106fe7ae955392df3609e17e4a487d34
lmorv <kishongus@gmail.com>
2023-02-07 10:25:42 -0500

    Particles: Got to display a single sparkle on the screen!

--- winter-23/Project1-ParticlesWithPersonalities/js/script.js
+++ winter-23/Project1-ParticlesWithPersonalities/js/script.js
@@ -30,7 +30,7 @@ Description of setup
 function setup() {
     createCanvas(windowWidth, windowHeight); 
 
-    sparkle = new Sparkle(mouseX,MouseY);
+    sparkle = new Sparkle(width/2, height/2);
 
 }
 
@@ -40,18 +40,17 @@ Description of draw()
 */
 function draw() {
   
-    setInterval(changeColor, 4000);
-  
-    for 
     
-
-
+    sparkle.display();
+    
+    setInterval(changeColor, 4000);
 //    console.log(enviroColor.b);
 }
 
 
 function changeColor() {
-    // console.log('changing color!');
+    console.log('changing color!'); 
+
     let water = color(30,100,200);
     let fire = color(200,30,100);
     let forest = color(30,200,100);
@@ -63,8 +62,10 @@ function changeColor() {
 
     if(currentColor === water ) {
         let counter = 4;
-            for(let i = 0; i < counter; i++) {  
-                lerpColor(water,forest, i);
+
+        for(let i = 0; i < counter; i++) {  
+                let counterToRange = map(i,0, counter, 0, 1); // I think this should convert the range of 0 to the counter's value to a 0 to 1 range...
+                lerpColor(water,forest, i); // and using the mapped value here should increase the lerp amount for every tick of the counter... in theory.
             }
     }
    
@@ -79,14 +80,14 @@ class Sparkle {
         // this.vel.mult(random(1,5));
         this.acc = createVector(0,0);
         this.lifetime = 255;
-        this.dim = 4;
+        this.dim = 100;
     }
     
     display () {
         //display some shiny sparks
         noStroke();
         fill(255);
-        square(this.x,this.y, this.dim, 20, 0, 20, 0);
+        square(this.x,this.y, this.dim, 40, 0, 40, 0); // Square with some rounded corners.
         
     }
 }
